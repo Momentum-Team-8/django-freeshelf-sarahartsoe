0bac70a2ab583c565fee974cd3595c7b58c67fb3
sarahartsoe <sarahartsoe@gmail.com>
2021-06-21 17:08:47 -0400

    created book form and html

--- books/models.py
+++ books/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from django.utils import timezone
 
 class User(AbstractUser):
     def __repr__(self):
@@ -8,3 +8,10 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    description = models.CharField(max_length=600)
+    created_date = models.DateTimeField(default=timezone.now)

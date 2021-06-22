ff9b45cbd9a6215649370c0e2356388e2742eb1b
sarahartsoe <sarahartsoe@gmail.com>
2021-06-22 18:19:09 -0400

    updated some ui/ux features

--- books/views.py
+++ books/views.py
@@ -2,19 +2,23 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Book
 from .forms import BookForm
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def homepage(request):
     # show a homepage
     return render(request, "books/homepage.html")
 
+@login_required
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'books/book_list.html', {'books': books})
 
+@login_required
 def profile_page(request):
     return render(request, "books/profile_page.html")
 
+@login_required
 def book_edit(request, pk):
     book = get_object_or_404(Book, pk=pk)
     if request.method == "POST":
@@ -27,3 +31,8 @@ def book_edit(request, pk):
     else:
         form = BookForm(instance=book)
     return render(request, 'books/book_edit.html', {'book': book})
+
+def book_delete(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    book.delete()
+    return redirect('book_list')

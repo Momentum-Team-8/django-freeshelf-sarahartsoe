cd30bc5f2fdb9d7c27ce3be007dd1f50588054a8
sarahartsoe <sarahartsoe@gmail.com>
2021-06-22 12:05:22 -0400

    created homepage view

--- books/views.py
+++ books/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from .models import Book
 # Create your views here.
 
+def homepage(request):
+    # show a homepage
+    return render(request, "books/homepage.html")
+
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'books/book_list.html', {'books': books})
